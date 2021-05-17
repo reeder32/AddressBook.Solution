72bc1295ed642834d9b9780ad2e00ae070fc1a33
Nicholas Reeder <nickreeder32@gmail.com>
2021-05-17 11:54:33 -0700

    :truck: add getAll and clearAll functions to Contact model
    Co-authored-by: Sammai Gutierrez <sammaigtz@hotmail.com>

--- AddressBook/Models/Contact.cs
+++ AddressBook/Models/Contact.cs
@@ -22,6 +22,41 @@ namespace AddressBook.Models
       LastName = lastname;
       Id = id;
     }
-
+    public static List<Contact> GetAll()
+    {
+      List<Contact> allContacts = new List<Contact> { };
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM Contacts;";
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while (rdr.Read())
+      {
+        int ContactId = rdr.GetInt32(0);
+        string ContactFirstName = rdr.GetString(1);
+        string ContactLastName = rdr.GetString(2);
+        Contact newContact = new Contact(ContactFirstName, ContactLastName, ContactId);
+        allContacts.Add(newContact);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return allContacts;
+    }
+    public static void ClearAll()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM contacts;";
+      cmd.ExecuteNonQuery();
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
   }
 }
