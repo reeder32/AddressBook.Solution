83406de563b44d7fc0dc3799325d51d90935e2ed
Nicholas Reeder <nickreeder32@gmail.com>
2021-05-17 11:10:28 -0700

    :tada: init files and folders for sample db project using mysql
    Co-authored-by: Sammai Gutierrez <sammaigtz@hotmail.com>

---
+++ AddressBook.Tests/ModelTests/ContactTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+
+namespace AddressBook.Tests
+{
+  [TestClass]
+  public class ContactTests
+  {
+
+  }
+}
---
+++ AddressBook/Models/Contact.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using System;
+
+namespace AddressBook.Models
+{
+  public class Contact
+  {
+
+  }
+}
